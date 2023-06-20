af8ab3463f6a0914418695baa17d4c077b4a723d
up2018129 <up2018129@myport.ac.uk>
2023-06-20 11:25:06 +0100

    on click sometimes works for spawning capsules

--- GameJam2023/Assets/Scripts/TileSelector.cs
+++ GameJam2023/Assets/Scripts/TileSelector.cs
@@ -1,3 +1,4 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class TileSelector : MonoBehaviour
@@ -8,15 +9,31 @@ public class TileSelector : MonoBehaviour
 
     [SerializeField] MeshRenderer meshRenderer;
 
+    [SerializeField] GameObject unit;
+
+
 
     private void Awake()
     {
         thisMaterial = meshRenderer.material;//can be changed if need multiple materials
     }
 
+
+
+
+    
+
+
+
+
     private void OnMouseEnter()
     {
         meshRenderer.material = HoverMat;
+
+        if (Input.GetMouseButton(0))
+        {
+            Instantiate(unit,transform.position,Quaternion.identity);
+        }
     }
 
     private void OnMouseExit()
